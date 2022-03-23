5bde56184dafc2c1e4171cfb7ebbfb4d32a02229
jiwan9599 <97672454+jiwan9599@users.noreply.github.com>
2022-03-23 19:27:35 +0530

    Add files via upload

---
+++ First_day_Practice (1).py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+#print('''dncaindcinsindciniasdiidiccnisndccnisndiccniandinidicninidcnidicdicnidicnindicnisdicindcin''')
+print("Hello World")
+# To print hello world and commenting out a single line.
+
+
+# In[9]:
+
+
+#How to find the value of pie.
+a=22
+b=7
+c=a/b
+print(c)
+
+
+# In[10]:
+
+
+#Practice of Arithematic Operaters
+a=22
+b=11
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+
+# In[ ]:
+
+
+
+
